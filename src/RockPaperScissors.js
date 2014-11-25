a596cc818690232ffb5f55cde3d3b9bc57aa919c
Alan Bridger <alanrbridger@gmail.com>
2014-11-25 16:06:56 +0000

    Refactors game logic.

--- src/RockPaperScissors.js
+++ src/RockPaperScissors.js
@@ -11,16 +11,14 @@ function Game(player1, player2) {
   this.player2 = player2;
 };
 
-var winningCombinations = {"rock" : ["scissors", "lizard"],
-                           "scissors" : ["paper", "lizard"],
-                           "paper" : ["rock", "spock"],
-                           "lizard" : ["paper", "spock"],
-                           "spock" : ["rock", "scissors"]}
+var pairs = {"rock" : ["scissors", "lizard"],
+             "scissors" : ["paper", "lizard"],
+             "paper" : ["rock", "spock"],
+             "lizard" : ["paper", "spock"],
+             "spock" : ["rock", "scissors"]}
 
 Game.prototype.winner = function(){
-  player1pick = winningCombinations[this.player1.pick]
-  player2pick = winningCombinations[this.player2.pick]
-  if (((player1pick[0] == this.player2.pick) || (player1pick[1]) == this.player2.pick)) return this.player1
-  if (((player2pick[0] == this.player1.pick) || (player2pick[1]) == this.player1.pick)) return this.player2
+  if (pairs[this.player1.pick].indexOf(this.player2.pick) > -1 ) return this.player1
+  if (pairs[this.player2.pick].indexOf(this.player1.pick) > -1 ) return this.player2
   if (this.player1.pick == this.player2.pick) return null
 };
