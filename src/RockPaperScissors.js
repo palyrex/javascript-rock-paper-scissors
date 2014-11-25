d92ac9f17f42f3c26842d0140e8f0956bb68fbe5
Alan Bridger <alanrbridger@gmail.com>
2014-11-25 11:56:40 +0000

    Adds draw logic.

--- src/RockPaperScissors.js
+++ src/RockPaperScissors.js
@@ -12,6 +12,7 @@ function Game(player1, player2) {
 };
 
 Game.prototype.winner = function () {
+  if (this.player1.pick === this.player2.pick) return null
   if (this.player1.pick === 'rock' && this.player2.pick === 'scissors') return this.player1;
   if (this.player1.pick === 'scissors' && this.player2.pick === 'paper') return this.player1;
   if (this.player1.pick === 'paper' && this.player2.pick === 'rock') return this.player1;
