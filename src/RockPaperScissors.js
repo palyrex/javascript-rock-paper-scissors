b11f92f12a08986d1d8333a53beb7d259dea1e18
Alan Bridger <alanrbridger@gmail.com>
2014-11-26 11:59:28 +0000

    Logic changed to correct verb hash lookup problems.

--- src/RockPaperScissors.js
+++ src/RockPaperScissors.js
@@ -22,37 +22,45 @@ function Game(player1, player2) {
   this.player2 = player2;
 };
 
-var pairs = {"rock"     : ["scissors", "lizard" ],
-             "scissors" : ["paper", "lizard"    ],
-             "paper"    : ["rock", "spock"      ],
-             "lizard"   : ["paper", "spock"     ],
-             "spock"    : ["rock", "scissors"   ]}
+Game.prototype.PAIRS = {
+  rock:     { scissors: 'crushes', lizard: 'squashes' },
+  paper:    { rock: 'covers', spock: 'disproves' },
+  scissors: { paper: 'cuts', lizard: 'decapitates' },
+  lizard:   { spock: 'poisons', paper: 'eats' },
+  spock:    { rock: 'vaporises', scissors: 'smashes' }
+}
 
-Game.prototype.winner = function(){
-  if (pairs[this.player1.pick].indexOf(this.player2.pick) > -1 ) return this.player1
-  if (pairs[this.player2.pick].indexOf(this.player1.pick) > -1 ) return this.player2
-  if (this.player1.pick == this.player2.pick) return null
-};
+Game.prototype.winner = function() {
+  if(this._isSamePick()) return null;
 
-Game.prototype.loser = function(){
-  if (pairs[this.player1.pick].indexOf(this.player2.pick) > -1 ) return this.player2
-  if (pairs[this.player2.pick].indexOf(this.player1.pick) > -1 ) return this.player1
-  if (this.player1.pick == this.player2.pick) return null
-};
+  if(this._victoryVerbFor(this.player1.pick, this.player2.pick)) {
+    return this.player1;
+  }
+  else {
+    return this.player2;
+  }
+}
 
-Game.prototype.message = function() {
-if (this.winner() === null) return "That's a DRAW"
-verbs = {  "paper"    : ["covers","rock"        ],
-           "rock"     : ["crushes", "lizard"    ],
-           "lizard"   : ["poisons", "spock"     ], 
-           "spock"    : ["smashes", "scissors"  ],
-           "scissors" : ["cuts", "paper"        ],
-           "paper"    : ["disproves", "spock"   ],
-           "lizard"   : ["eats", "paper"        ],
-           "paper"    : ["disproves", "spock"   ],
-           "spock"    : ["vaporises", "rock"    ],
-           "rock"     : ["crushes", "scissors"  ]}
-if (verbs[this.winner().pick].indexOf(this.loser().pick) > -1) verb = verbs[this.winner().pick][0];
-return (this.winner().name + "'s " + this.winner().pick + " " + verb + " " + this.loser().name + "'s " + this.loser().pick);
+Game.prototype.loser = function() {
+  return (this.winner() === this.player1 ? this.player2 : this.player1);
+}
 
-};
+Game.prototype.winningMessage = function() {
+  var message;
+
+  if(this.winner()) {
+    message = this.winner().name + "'s " + this.winner().pick + " " + this._victoryVerbFor(this.winner().pick, this.loser().pick) + " " + this.loser().name + "'s " + this.loser().pick;
+  } else {
+    message = 'Draw';
+  }
+
+  return message;
+}
+
+Game.prototype._victoryVerbFor = function(pick, opponentPick) {
+  return this.PAIRS[pick][opponentPick];
+}
+
+Game.prototype._isSamePick = function() {
+  return this.player1.pick === this.player2.pick;
+}
