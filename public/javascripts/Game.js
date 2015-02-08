a28a381f336bd1d2503f34ff0956741ddfc85438
Alan Bridger <alanrbridger@gmail.com>
2015-02-08 11:07:05 +0000

    More tidying and adding to readme

--- public/javascripts/Game.js
+++ public/javascripts/Game.js
@@ -1,7 +1,7 @@
 function Game(player1, player2) {
   this.player1 = player1;
   this.player2 = player2;
-};
+}
 
 Game.prototype.PAIRS = {
   rock:     { scissors: 'crushes', lizard: 'squashes' },
@@ -9,21 +9,21 @@ Game.prototype.PAIRS = {
   scissors: { paper: 'cuts', lizard: 'decapitates' },
   lizard:   { spock: 'poisons', paper: 'eats' },
   spock:    { rock: 'vaporises', scissors: 'smashes' }
-}
+};
 
 Game.prototype.winner = function() {
-  if(this._isSamePick()) return null;
+  if(this._isSamePick()) { return null };
   if(this._victoryVerbFor(this.player1.pick, this.player2.pick)) {
     return this.player1;
   }
   else {
     return this.player2;
   }
-}
+};
 
 Game.prototype.loser = function() {
   return (this.winner() === this.player1 ? this.player2 : this.player1);
-}
+};
 
 Game.prototype.winningMessage = function() {
   var message;
@@ -33,12 +33,12 @@ Game.prototype.winningMessage = function() {
     message = 'Draw';
   }
   return message;
-}
+};
 
 Game.prototype._victoryVerbFor = function(pick, opponentPick) {
   return this.PAIRS[pick][opponentPick];
-}
+};
 
 Game.prototype._isSamePick = function() {
   return this.player1.pick === this.player2.pick;
-}
+};
